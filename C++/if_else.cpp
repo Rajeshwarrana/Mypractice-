b160751fae348a2f33bf825e6ac9a893ed862216
Rajeshwarrana <rajeshwarrana09@gmail.com>
2025-07-26 20:08:25 +0530

    Basic code using if else

--- C++/if_else.cpp
+++ C++/if_else.cpp
@@ -2,4 +2,21 @@
 
 int main(){
     
+    int num1 {20};
+    int num2 {50};
+
+    bool result = (num1 > num2);
+
+    std :: cout << std :: boolalpha << "result :" << result << std :: endl;
+    std :: cout << std :: endl;
+
+    if (result == true){
+        std :: cout << num1 << " is greater than " << num2 << std :: endl;
+    }
+    if (!(result == true)){
+        std :: cout << num1 << " is not greater than " << num2 << std :: endl;
+    }
+
+    return 0;
+
 }
